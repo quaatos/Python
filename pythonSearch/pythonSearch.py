a43047493808a20df706f0e4fabcd2c42c2183ad
quaatos <93578678+quaatos@users.noreply.github.com>
2022-03-21 11:26:14 +0100

    Update pythonSearch.py

--- pythonSearch/pythonSearch.py
+++ pythonSearch/pythonSearch.py
@@ -1,11 +1,11 @@
 import webbrowser
 
 #script asks the user were and what he want to search 
-print("options are: youtube, wikipedia, google,")
+print("options are: youtube, wikipedia, google, DuckDuckGo")
 options = input("where do you want to search: ")
 
 #checks if the input is valid.
-if options != "wikipedia" and options != "google" and options != "youtube":
+if options != "wikipedia" and options != "google" and options != "youtube" and options != "DuckDuckGo":
     print ("Enter a correct input")
     exit()
 
@@ -18,13 +18,16 @@ if options == "google":
     print ("this the result on google for:", question)
     webbrowser.open("https://google.com/search?q={}".format(question))
 
-if options == "youtube":
+elif options == "youtube":
     print ("This is the result on youtube for:", question)
     webbrowser.open("https://youtube.com/search?q={}".format(question))
 
-if options == "wikipedia":
+elif options == "wikipedia":
     print ("This is the result on wikipedia for:", question)
     webbrowser.open("https://wikipedia.org/wiki/{}".format(question))
 
+elif options == "DuckDuckGo":
+    print ("This is the result on DuckDuckGo for:", question)
+    webbrowser.open("https://duckduckgo.com/?t=ffab&q={}".format(question))
 
 #developed by Quaatos with <3
