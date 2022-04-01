112b80d40a6651b9e92d500a773744faa709a8bf
quaatos <93578678+quaatos@users.noreply.github.com>
2022-04-01 23:52:35 +0200

    Update discordBot.py

--- slykaBot/discordBot.py
+++ slykaBot/discordBot.py
@@ -2,12 +2,15 @@ import discord
 import random
 import datetime
 import string
-TOKEN = "DISCORD TOKEN"
+
+#from setuptools import Command
+TOKEN = "YOUR TOKEN"
 
 dateTime = datetime.datetime.now()
 MonthDay = dateTime.strftime("%d")
 Month = dateTime.strftime("%B")
 
+#open file and take a random sentence
 with open('facts.txt') as f:
     lines = f.readlines()
 
@@ -20,7 +23,7 @@ async def on_message(message):
 
     if '!help' == message.content.lower():
         await message.channel.send('\n `!help` --- Print available commands\n `!fact` --- Print a random fact\n `!ryt` --- random youtube link\n `!rph` --- random pornhub link')
-        await message.channel.send(' `!date` --- get the date of today')
+        await message.channel.send(' `!date` --- get the date of today\n `!source` --- my source code')
 
     if '!fact' == message.content.lower():
         await message.channel.send(random.choice(lines))
@@ -33,12 +36,18 @@ async def on_message(message):
         ran = ''.join(random.choices(string.ascii_lowercase + string.digits, k = 13))
         await message.channel.send("https://pornhub.com/view_video.php?viewkey=ph" + str(ran))
 
-    if '!time' == message.content.lower():
+    if '!date' == message.content.lower():
         await message.channel.send("date: " + Month + ' ' + MonthDay)
-    
-    
+
+    if '!source' == message.content.lower():
+        await message.channel.send("https://github.com/quaatos/Python/tree/main/slykaBot")
+
 client.run(TOKEN)
 
 # ------ IDEAS ------- #
 # 
 #
+
+#developed with love by quaatos and skaffa
+#https://github.com/skaffa
+#https://github.com/quaatos
