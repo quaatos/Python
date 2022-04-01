98afa10541c956d28cba2ee1febd3c1f116a7e2c
quaatos <93578678+quaatos@users.noreply.github.com>
2022-04-01 23:28:04 +0200

    Update discordBot.py

--- slykaBot/discordBot.py
+++ slykaBot/discordBot.py
@@ -1,16 +1,15 @@
 import discord
 import random
 import datetime
-TOKEN = "TOKEN"
+import string
+TOKEN = "OTU5NTQyNjg0Nzk4ODIwMzcz.YkdZww.R-gg2z3HoCyk2QPzR2sgBeZUgH4"
 
 dateTime = datetime.datetime.now()
+MonthDay = dateTime.strftime("%d")
+Month = dateTime.strftime("%B")
 
-facts = [
-    'Plutonium is radioactive',
-    '',
-    'tungsten is the thoughest metal known on earth',
-    '',
-]
+with open('facts.txt') as f:
+    lines = f.readlines()
 
 client = discord.Client()
 
@@ -19,17 +18,27 @@ async def on_message(message):
     if message.author.bot: #bot won't respond to their own message
         return
 
-    if '!help' is message.content.lower():
-        await message.channel.send('porno')
-        
-    if '!fact' is message.content.lower():
-        
- 
- 
+    if '!help' == message.content.lower():
+        await message.channel.send('\n `!help` --- Print available commands\n `!fact` --- Print a random fact\n `!ryt` --- random youtube link\n `!rph` --- random pornhub link')
+        await message.channel.send(' `!date` --- get the date of today')
 
+    if '!fact' == message.content.lower():
+        await message.channel.send(random.choice(lines))
+
+    if '!ryt' == message.content.lower():
+        ran = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits + "-_", k = 11))
+        await message.channel.send("https://youtu.be/watch?v=" + str(ran))
+
+    if '!rph' == message.content.lower():
+        ran = ''.join(random.choices(string.ascii_lowercase + string.digits, k = 13))
+        await message.channel.send("https://pornhub.com/view_video.php?viewkey=ph" + str(ran))
+
+    if '!time' == message.content.lower():
+        await message.channel.send("date: " + Month + ' ' + MonthDay)
+    
+    
 client.run(TOKEN)
 
-
 # ------ IDEAS ------- #
-# let the script read a text file and take a random fact out of it
+# 
 #
